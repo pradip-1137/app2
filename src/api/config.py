d4d2e925613f812cb714d177f01ecf4d8555a4cf
pradip-1137 <96612151+pradip-1137@users.noreply.github.com>
2023-09-18 00:39:04 +0530

    Update config.py

--- src/api/config.py
+++ src/api/config.py
@@ -2,9 +2,9 @@ import os
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "dev")
 DEBUG = ENVIRONMENT == "dev"
-HOST = '0.0.0.0' if ENVIRONMENT == "prod" else 'localhost'
-POSTGRES_DB = os.environ.get("POSTGRES_DB", "postgres")
-POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "password")
-POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
+HOST = '0.0.0.0' if ENVIRONMENT == "prod" else ''
+POSTGRES_DB = os.environ.get("POSTGRES_DB", "mydb")
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "pradip")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "Redhat@1137")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
